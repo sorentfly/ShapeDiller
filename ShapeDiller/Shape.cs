2c840db6993bbd4eea743503b5cb2b624f8fedaf
Кузнецов Владислав Владимирович <kvv.16@lore.uni-dubna.ru>
2018-09-04 15:29:49 +0300

    Добавьте файлы проекта.

---
+++ ShapeDiller/Shape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace ShapeDiller
+{
+    class Shape
+    {
+        #region ATTRIBUTES
+
+        /* Цвет оконтовки */
+        private Enum Color { get; set; }
+        /* Цвет заливки */
+        private Enum FillColor { get; set; }
+        /* Координата X (центр) */
+        private int X { get; set; }
+        /* Координата Y (центр) */
+        private int Y { get; set; }
+        /* Длина фигуры */
+        private int Width { get; set; }
+        /* Высота фигуры */
+        private int Height { get; set; }
+
+        #endregion
+        
+        /**
+         * 
+         * */
+        public Shape()
+        {
+
+        }
+
+        
+    }
+}
